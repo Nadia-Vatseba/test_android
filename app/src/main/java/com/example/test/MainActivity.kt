b3b84e7f801d2a23dc4666e0fac586052ff40fd5
Nadia Vatseba <vaceba28@gmail.com>
2025-04-13 21:04:54 +0300

    remove

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -16,7 +16,6 @@ import com.example.test.ui.theme.TestTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             TestTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
