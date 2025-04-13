1c75a306b56e9850ab0fe7141579b9001bbb6d7f
Nadia Vatseba <vaceba28@gmail.com>
2025-04-13 20:42:19 +0300

    value commet

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -16,7 +16,7 @@ import com.example.test.ui.theme.TestTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        //enableEdgeToEdge()
         setContent {
             TestTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
